e42fcc872560a8571f83401aac31c7f02b0243db
xiayong <i.xiayong@me.com>
2018-04-17 23:32:39 +0800

    upgrade the spring cloud version.

--- example-springcloud-provider/src/main/java/me/xiayong/example/springcloud/provider/ExampleProviderApplication.java
+++ example-springcloud-provider/src/main/java/me/xiayong/example/springcloud/provider/ExampleProviderApplication.java
@@ -2,14 +2,14 @@ package me.xiayong.example.springcloud.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author YongXia.
  * @since 1.0
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ExampleProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExampleProviderApplication.class, args);
